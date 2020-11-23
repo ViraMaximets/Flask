a92a762a1266326a8797c9a409966482800438cc
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2020-11-23 13:02:35 +0200

    Deleted unused files

--- python/swagger_client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from swagger_client.api.admin_api import AdminApi
-from swagger_client.api.car_api import CarApi
-from swagger_client.api.store_api import StoreApi
-from swagger_client.api.user_api import UserApi
